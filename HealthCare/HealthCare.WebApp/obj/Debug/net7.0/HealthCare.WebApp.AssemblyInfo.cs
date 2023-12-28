df381af011302cfd8eb469f254e2a3fa2f1f8807
paulcarter14 <paulcarter190410@gmail.com>
2023-12-28 17:20:12 +0100

    authentication added.

--- HealthCare/HealthCare.WebApp/obj/Debug/net7.0/HealthCare.WebApp.AssemblyInfo.cs
+++ HealthCare/HealthCare.WebApp/obj/Debug/net7.0/HealthCare.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19dfd3312d844c912e61049b7aea482d4992b9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
