0413d51c6c2d47b7b5c5464bc1204967b383f8eb
annabjurback <annabjurback@gmail.com>
2024-01-04 14:10:27 +0100

    startup personcontroller

--- HealthCare/HealthCare.Core/Controllers/PersonController.cs
+++ HealthCare/HealthCare.Core/Controllers/PersonController.cs
@@ -7,7 +7,19 @@ using System.Web.Mvc;
 
 namespace HealthCare.Core.Controllers
 {
-	public class PersonController : Controller
+	public class PersonController : Controller //: IHealthcareController
 	{
+		private readonly HealthcareContext _context;
+
+		public PersonController(HealthcareContext context)
+		{
+			_context = context;
+		}
+	}
+
+	//[HttpGet("/person")]   //eller?
+	public Person GetPerson(int id)
+	{
+		// men, vi ska kanske inte skicka HELA personobjektet...?
 	}
 }
