db9f4264be4c0349fc3a4e4dd0c79e9585397957
Max Hansen <meepmopo@gmail.com>
2024-01-16 15:32:53 +0100

    Refactored caregiver controller

---
+++ HealthCare/HealthCare.Core/Repositories/CaregiverRepository.cs
@@ -0,0 +1,46 @@
+﻿using HealthCare.Core.Context;
+using HealthCare.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Core.Repositories
+{
+	internal class CaregiverRepository : ICaregiverRepository
+	{
+		private readonly HealthcareContext _context;
+        public CaregiverRepository(HealthcareContext context)
+        {
+			_context = context;
+        }
+        public void CreateCaregiver(Caregiver caregiver)
+		{
+			_context.Caregivers.Add(caregiver);
+			_context.SaveChanges();
+		}
+
+		public Caregiver EditCaregiver(string id, string firstName, string lastName, string role)
+		{
+			var caregiver = _context.Caregivers.Single(x => x.CaregiverId == id);
+			caregiver.FirstName = firstName;
+			caregiver.LastName = lastName;
+			caregiver.Role = role;
+
+			_context.SaveChanges();
+			return caregiver;
+		}
+
+		public List<Caregiver> GetAll()
+		{
+			return _context.Caregivers.ToList();
+		}
+
+		public Caregiver GetCaregiver(string id)
+		{
+			return _context.Caregivers.Single(x => x.CaregiverId == id);
+		}
+	}
+}
