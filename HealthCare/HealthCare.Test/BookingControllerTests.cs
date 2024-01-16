9feb561bb92844c987d0da814ae30d81742c5530
Max Hansen <meepmopo@gmail.com>
2024-01-16 16:07:29 +0100

    Setup booking test class

---
+++ HealthCare/HealthCare.Test/BookingControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using HealthCare.Core.Controllers;
+using HealthCare.Core.Repositories;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Test
+{
+	public class BookingControllerTests
+	{
+		private readonly BookingController _sut;
+		private readonly IBookingRepository _bookingRepository = Substitute.For<IBookingRepository>();
+
+        public BookingControllerTests()
+        {
+            _sut = new BookingController(_bookingRepository);
+        }
+    }
+}
